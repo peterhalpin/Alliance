576a752f11cb75729c17bf857385bda235b33d29
Rohaid Bakheet <rohaidb1999@gmail.com>
2020-03-24 15:39:56 -0400

    working on pressure switches

--- Assets/Scripts/Switch.cs
+++ Assets/Scripts/Switch.cs
@@ -3,7 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+//Overall C# Notes
+  /*No Hashmaps, Only Dictionary
+  *Tilemaps =/= Game Objects
+  *setting scopes of variables is complicated
+  */
 
+  //Pressure Switch, 
+    //Have it continously check if the Box/Strength is touching  in the update function
+      //if not then set the boolean value to false and have the tilemap the same
+      //if it is then "hide" the collider 2D
 public class Switch : MonoBehaviour
 {
 [SerializeField]
@@ -15,11 +24,10 @@ public class Switch : MonoBehaviour
     public GameObject SwitchOn { get => switchOn; set => switchOn = value; }
     public GameObject SwitchOff { get => switchOff; set => switchOff = value; }
     private Dictionary<string, Tilemap> tilemapses = new Dictionary<string, Tilemap>();
-    private string id;
+  
 
     void Start() {
         // code to have the switch in default appearance
-     
         gameObject.GetComponent<SpriteRenderer>().sprite = SwitchOff.GetComponent<SpriteRenderer>().sprite;
         
           var tilemaps = new Tilemap[3];
@@ -30,27 +38,25 @@ public class Switch : MonoBehaviour
           }
     }
     void OnTriggerEnter2D(Collider2D col) {
-        //changes switch to "on"
-        Debug.Log("Test2");
+        //changes switch to "on" appearance
         gameObject.GetComponent<SpriteRenderer>().sprite = SwitchOn.GetComponent<SpriteRenderer>().sprite;
-        Debug.Log("Game ID"+ gameObject.GetInstanceID());
-        id = gameObject.name;
-         
+
+        string id = gameObject.name;
+        Tilemap ty;
+        Debug.Log(col.name);
         if("TL_P_Switch" == id) {
-         Tilemap ty = tilemapses["TL_Wall"];
-         Debug.Log("Tilemap" + ty.name);
+         ty =  tilemapses["TL_Wall"];
+        //  Debug.Log("Tilemap" + ty.name);
          ty.ClearAllTiles();
         } else if("TR_P_Switch" == id) {
-         Tilemap ty = tilemapses["BR_Wall"];
-         Debug.Log("Tilemap" + ty.name);
+         ty = tilemapses["BR_Wall"];
+        //  Debug.Log("Tilemap" + ty.name);
          ty.ClearAllTiles();
         } else if ("BL_NP_Switch" == id){
-         Tilemap ty = tilemapses["C_Wall"];
-         Debug.Log("Tilemap" + ty.name);
+         ty = tilemapses["C_Wall"];
+        //  Debug.Log("Tilemap" + ty.name);
          ty.ClearAllTiles();
         }
-        
-        print("Delete Wall");
         
         isOn = true;
     }
