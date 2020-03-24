8f6c0061fdd668bfe3fa0e2674693801104f3d73
Manuel Cabrejos <manuelcabrejos97@gmail.com>
2020-03-24 18:23:38 -0400

    set positions of each character in the right position

--- Assets/Scripts/MultiplayerNetwork/GameSetupController.cs
+++ Assets/Scripts/MultiplayerNetwork/GameSetupController.cs
@@ -16,14 +16,21 @@ public class GameSetupController : MonoBehaviour
     {
         Debug.Log("Creating Player");
         System.Random random = new System.Random();
-        int num = random.Next(1, 3);
+        int num = random.Next(1, 5);
         if (num == 1) {
-            PhotonNetwork.Instantiate(Path.Combine("Prefabs", "blek"), Vector3.zero, Quaternion.identity);
+            PhotonNetwork.Instantiate(Path.Combine("Prefabs", "blek"), new Vector3(-9, 6, 1), Quaternion.identity);
+        } else if (num == 2){
+            PhotonNetwork.Instantiate(Path.Combine("Prefabs", "blue"), new Vector3(9, -6, 1), Quaternion.identity);
+        } else if (num == 3){
+            PhotonNetwork.Instantiate(Path.Combine("Prefabs", "green"), new Vector3(9, 6, 1), Quaternion.identity);
         } else {
-            PhotonNetwork.Instantiate(Path.Combine("Prefabs", "blue"), Vector3.zero, Quaternion.identity);
+            PhotonNetwork.Instantiate(Path.Combine("Prefabs", "red"), new Vector3(-9, -6, 1), Quaternion.identity);
         }
 
         // PhotonNetwork.Instantiate(Path.Combine("Prefabs", "blek"), Vector3.zero, Quaternion.identity);
+        // PhotonNetwork.Instantiate(Path.Combine("Prefabs", "blek"), new Vector3(-9, 6, 1), Quaternion.identity);
+
+        
     }
 
 }
