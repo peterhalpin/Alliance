81b9f206110dd103203c6a62b3c1b8ab5873962e
Manuel Cabrejos <manuelcabrejos97@gmail.com>
2020-03-24 15:01:33 -0400

    Added Photon, multiplayer folder in scripts with content, Resources foulder, renamed prefabs2 to prefabs and moved into resources, and added photon view and photon transform to each player

---
+++ Assets/Scripts/MultiplayerNetwork/GameSetupController.cs
@@ -0,0 +1,29 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class GameSetupController : MonoBehaviour
+{
+    // This script will be added to any multiplayer scene
+    void Start()
+    {
+        CreatePlayer(); // Create a networked player object for each player that loads into the multiplayer scenes.
+    }
+
+    private void CreatePlayer()
+    {
+        Debug.Log("Creating Player");
+        System.Random random = new System.Random();
+        int num = random.Next(1, 3);
+        if (num == 1) {
+            PhotonNetwork.Instantiate(Path.Combine("Prefabs", "blek"), Vector3.zero, Quaternion.identity);
+        } else {
+            PhotonNetwork.Instantiate(Path.Combine("Prefabs", "blue"), Vector3.zero, Quaternion.identity);
+        }
+
+        // PhotonNetwork.Instantiate(Path.Combine("Prefabs", "blek"), Vector3.zero, Quaternion.identity);
+    }
+
+}
