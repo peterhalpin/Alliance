024ce6db2cc16bad88a3040c08befd37f73bb0b4
Manuel Cabrejos <manuelcabrejos97@gmail.com>
2020-03-31 16:09:27 -0400

    github readded

---
+++ Assets/Scripts/MultiplayerNetwork/GameSetups/Level2_GSC.cs
@@ -0,0 +1,20 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Level2_GSC : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
