d86e501b813d05602499ddd8a2c0b4f3219f6275
Manuel Cabrejos <manuelcabrejos97@gmail.com>
2020-04-07 15:04:28 -0400

    adding isMine check on players

--- Assets/Scripts/StrengthController.cs
+++ Assets/Scripts/StrengthController.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StrengthController : MonoBehaviour
+public class StrengthController : MonoBehaviourPun
 {
     public float speed = 2.5f;
     // Start is called before the first frame update
@@ -13,17 +14,34 @@ public class StrengthController : MonoBehaviour
    // Update is called once per frame
    void Update()
    {
-      if (Input.GetKey("a")){
-         transform.position += Vector3.left * speed * Time.deltaTime;
+       if (photonView.IsMine == false && PhotonNetwork.IsConnected == true) {
+            return;
         }
-     if (Input.GetKey("d")){
-         transform.position += Vector3.right * speed * Time.deltaTime;
+
+
+        if (Input.GetKey(KeyCode.LeftArrow)){
+            transform.position += Vector3.left * speed * Time.deltaTime;
         }
-     if (Input.GetKey("w")){
-         transform.position += Vector3.up * speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.RightArrow)){
+            transform.position += Vector3.right * speed * Time.deltaTime;
         }
-     if (Input.GetKey("s")){
-         transform.position += Vector3.down * speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.UpArrow)){
+            transform.position += Vector3.up * speed * Time.deltaTime;
         }
+        if (Input.GetKey(KeyCode.DownArrow)){
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+    //   if (Input.GetKey("a")){
+    //      transform.position += Vector3.left * speed * Time.deltaTime;
+    //     }
+    //  if (Input.GetKey("d")){
+    //      transform.position += Vector3.right * speed * Time.deltaTime;
+    //     }
+    //  if (Input.GetKey("w")){
+    //      transform.position += Vector3.up * speed * Time.deltaTime;
+    //     }
+    //  if (Input.GetKey("s")){
+    //      transform.position += Vector3.down * speed * Time.deltaTime;
+    //     }
    }
 }
