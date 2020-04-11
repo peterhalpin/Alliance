0df8969beac4a176b487b71ef4ca6cca32aa9101
Manuel Cabrejos <manuelcabrejos97@gmail.com>
2020-04-11 00:22:36 -0400

    players load into level 2 but for some reason are behind the map...working on fix

--- Assets/Scripts/FinalSwitch.cs
+++ Assets/Scripts/FinalSwitch.cs
@@ -1,11 +1,21 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class FinalSwitch : MonoBehaviour
+public class FinalSwitch : MonoBehaviourPun
 {
     private List<Collider2D> colliders = new List<Collider2D>();
+    private InfoObject infoObject;
+    private PhotonView myPhotonView;
+
+    private void Awake() {
+        infoObject = GameObject.FindObjectOfType<InfoObject>();
+        myPhotonView = GetComponent<PhotonView>();
+    }
+
 
     void OnTriggerEnter2D(Collider2D player)
     {
@@ -13,8 +23,13 @@ public class FinalSwitch : MonoBehaviour
             colliders.Add(player);
         }
       
-        if(colliders.Count ==4){
-            SceneManager.LoadScene(sceneName:"Level2");
+        if(colliders.Count == 2){
+            if(PhotonNetwork.IsMasterClient){
+                myPhotonView.RPC("LevelCountUpdate", RpcTarget.All, true);
+            }
+            SceneManager.LoadScene(sceneName: "Level2");
+            Debug.Log("shit again");
+            // myPhotonView.RPC("LoadNextLevel", RpcTarget.All, "Level2");
         }
 
     }
@@ -22,4 +37,21 @@ public class FinalSwitch : MonoBehaviour
     void OnTriggerExit2D(Collider2D player){
         colliders.Remove(player);
     }
+
+
+    [PunRPC]
+    private void LevelCountUpdate(bool goingToNextLevel) {
+        // updates on all players what level we're on
+        infoObject.UpdateLevel(goingToNextLevel);
+    }
+
+    // [PunRPC]
+    // private void LoadNextLevel(string levelName) {
+    //     SceneManager.LoadScene(sceneName: levelName);
+
+    // }
+
+
 }
+
+
--- Assets/Scripts/MultiplayerNetwork/NetworkController.cs
+++ Assets/Scripts/MultiplayerNetwork/NetworkController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class NetworkController : MonoBehaviourPunCallbacks
 {
+
+    
     // Start is called before the first frame update
     void Start() {
         PhotonNetwork.ConnectUsingSettings(); //Connects to Photon master servers
