66dac95a46a4e0928cd5147527f39d2984f6ddbc
Rohaid Bakheet <rohaidb1999@gmail.com>
2020-04-28 15:57:45 -0400

    fixed rock?

--- Assets/Scripts/MultiplayerNetwork/GameSetupController.cs
+++ Assets/Scripts/MultiplayerNetwork/GameSetupController.cs
@@ -87,8 +87,8 @@ public class GameSetupController : MonoBehaviourPun
             Instantiate(varPrefab, new Vector3(0, 5, 0), Quaternion.identity);
             //  Object varPrefab2 = Resources.Load("Prefabs/Characters/blue", typeof(GameObject));
             // Instantiate(varPrefab2, new Vector3(3, 0, 0), Quaternion.identity);
-            //  Object varPrefab3 = Resources.Load("Prefabs/Characters/blek", typeof(GameObject));
-            // Instantiate(varPrefab3, new Vector3(0, 3, 0), Quaternion.identity);
+             Object varPrefab3 = Resources.Load("Prefabs/Characters/blek", typeof(GameObject));
+            Instantiate(varPrefab3, new Vector3(0, 3, 0), Quaternion.identity);
             //  Object varPrefab4 = Resources.Load("Prefabs/Characters/green", typeof(GameObject));
             // Instantiate(varPrefab4, new Vector3(0, 3, 0), Quaternion.identity);
             
