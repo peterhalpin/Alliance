03f6a6d59b2af7e3842cc78b0df884b83012dfb1
drose98 <dylan.a.rose@gmail.com>
2020-04-29 02:36:50 -0400

    added more comments to scripts

--- Assets/Scripts/Chat/LogHandler.cs
+++ Assets/Scripts/Chat/LogHandler.cs
@@ -12,23 +12,23 @@ public class LogHandler : MonoBehaviour
 
     public void Awake()
     {
-        DontDestroyOnLoad(transform.gameObject);
+        DontDestroyOnLoad(transform.gameObject);            //Preserves the gameobject and script throughout the game. 
     }
     
-    //Create new file for each team
+    //Create new file for each day. 
     public void CreateText(string teamName)
     {
 
         //Path of the file Assets/Resources/ChatLogs/chat{date}.txt
         path = Application.dataPath + "/Resources/ChatLogs/" + "chat" + DateTime.Now.ToString("yyyyMMdd") + ".txt";
 
-        if (!File.Exists(path))     //create new file if it doesnt e
+        if (!File.Exists(path))     //create new file if it doesnt exist
         {
-            File.WriteAllText(path, "Team: " + teamName + "\n\n");
+            File.WriteAllText(path, "Team: " + teamName + "\n\n");      //this method would erase any existing text, N/A in this scenario
         } 
         else                        // File already exists, create new section header
         {
-            File.AppendAllText(path, "\n\n" + "Team: " + teamName + "\n\n");
+            File.AppendAllText(path, "\n\n" + "Team: " + teamName + "\n\n");        //Adds to bottom of existing file
         }
 
     }
